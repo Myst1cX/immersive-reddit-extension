c0364b1e2443c5376568e8b597d244fa04489908
Myst1cX <97919309+Myst1cX@users.noreply.github.com>
2024-02-25 14:08:51 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -8,6 +8,12 @@ document.addEventListener('DOMContentLoaded', function() {
                     var currentUrl = tabs[0].url;
                     var modifiedUrl = 'read://https_www.reddit.com/?url=' + encodeURIComponent(currentUrl);
                     
+                    // Copy the modified link to the clipboard
+                    copyToClipboard(modifiedUrl);
+                    
+                    // Optionally, notify the user that the link is copied
+                    alert('Encoded link copied to clipboard:\n' + modifiedUrl);
+                    
                     // Open the modified link in a new tab
                     chrome.tabs.create({ url: modifiedUrl });
                 } catch (error) {
@@ -19,5 +25,12 @@ document.addEventListener('DOMContentLoaded', function() {
         console.error('encodeButton not found.');
     }
 
-   
+    function copyToClipboard(text) {
+        var textarea = document.createElement('textarea');
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+    }
 });
